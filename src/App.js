22a702b8d86fd9d46c287c7d63ea8e6687f2635f
rajeski <asiajsr@gmail.com>
2020-12-12 01:37:59 +0900

    Modified App.js / Cards.jsx and created api-folder and index.js file

--- src/App.js
+++ src/App.js
@@ -2,12 +2,25 @@ import React from 'react';
 
 import { Cards, Chart, CountryPicker } from './components';
 import styles from './App.module.css';
+import {fetchData } from './api';
 
 class App extends React.Component {
+    state = {
+        data: {},
+
+    }
+
+
+    async componentDidMount() {
+      const fetchData = await fetchData();
+
+      this.setState({ data: data })
+    }
     render() {
+        const { data } = this.state; 
         return (
             <div className={styles.container}>
-                <Cards />
+                <Cards data={data}/>
                 <CountryPicker />
                 <Chart />
             </div>
