ff31e6b5e4e9b04f85c0caa1e7dac683cb772370
rajeski <asiajsr@gmail.com>
2020-12-13 01:24:30 +0900

    Edited App.module.css; api/index.js; Cards.jsx; Chart.jsx; Chart.module.css; and, CountryPicker.jsx-files

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -2,6 +2,7 @@ import { configure } from '@testing-library/react';
 import React from 'react';
 import { Card, CardContent, Typography, Grid } from '@material-ui/core'; 
 import CountUp from 'react-countup';
+import cx from 'classnames';
 
 import styles from './Cards.module.css';
 
@@ -13,7 +14,7 @@ const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
     return (
         <div className={styles.container}>
             <Grid container spacing={3} justify='center'>
-                <Grid item component={Card}>
+                <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.infected)}>
                     <CardContent>
                         <Typography color='textSecondary' gutterBottom>Infected</Typography>
                         <Typography variant='h5'>
@@ -23,7 +24,7 @@ const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
                         <Typography variant='body2'>Number of active COVID-19 Cases</Typography>
                     </CardContent>
                 </Grid>
-                <Grid item component={Card}>
+                <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.recovered)}>
                     <CardContent>
                         <Typography color='textSecondary' gutterBottom>Recovered</Typography>
                         <Typography variant='h5'>
@@ -33,7 +34,7 @@ const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
                         <Typography variant='body2'>Number of recovered from COVID-19</Typography>
                     </CardContent>
                 </Grid>
-                <Grid item component={Card}>
+                <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.deaths)}>
                     <CardContent>
                         <Typography color='textSecondary' gutterBottom>Deaths</Typography>
                         <Typography variant='h5'>
--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import { NativeSelect, FormControl, StylesProvider } from '@material-ui/core';
+
+import styles from './CountryPicker.module.css';
 
 const CountryPicker = () => {
     return (
-        <h1>CountryPicker</h1>
+        <FormControl className={styles.FormControl}>
+            <NativeSelect>
+                <option value='global'></option>
+                 
+            </NativeSelect>
+        </FormControl>
     )
 }
 
